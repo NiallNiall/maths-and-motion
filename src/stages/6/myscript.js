f909eae1953266afeb3838b787af596272c49416
NiallNiall <niall.henn@gmail.com>
2016-02-16 00:14:07 +0000

    movers and shakers v2

--- src/stages/6/myscript.js
+++ src/stages/6/myscript.js
@@ -207,24 +207,47 @@
 
 var mainPaths = [];
 
-var points = [
-	view.center,
-	view.center
-	];
+// var points = [
+// 	view.center,
+// 	view.center
+// 	];
 
-var path = new Path(points);
+// var path = new Path(points);
 
-var mainPath = new Path(points);
-mainPath.strokeColor = 'red';
-mainPath.strokeWidth = 10.0;
-mainPath.strokeCap = 'round';
-mainPath.strokeJoin = 'round';
+// var mainPath = new Path(points);
+// mainPath.strokeColor = 'red';
+// mainPath.strokeWidth = 10.0;
+// mainPath.strokeCap = 'round';
+// mainPath.strokeJoin = 'round';
 
 var lineInc = 1;
 
 var rndpoint = view.center + (new Point(getRandomA(-1, 1),getRandomA(-1, 1))* 50);;
 // var myCircle = new Path.Circle(rndpoint, 10);
 // myCircle.fillColor = 'white';
+
+makeNewSquiggle();
+
+
+function makeNewSquiggle(){
+
+	var point1 = new Point(view.bounds.width, view.bounds.height) * Point.random();
+	var point2 = point1 + (new Point(getRandomA(-1, 1),getRandomA(-1, 1))* 50);
+	rndpoint = point2;
+
+	var points = [
+		point1,
+		point2
+	];
+
+	var path = new Path(points);
+
+	mainPaths.push( new Path(points));
+	mainPaths[mainPaths.length-1].strokeColor = Color.random();
+	mainPaths[mainPaths.length-1].strokeWidth = 10.0;
+	mainPaths[mainPaths.length-1].strokeCap = 'round';
+	mainPaths[mainPaths.length-1].strokeJoin = 'round';
+}
 
 function getRandomA(min, max) {
   return Math.random() * (max - min) + min;
@@ -239,7 +262,6 @@ function drawSquiggle(_path) {
 	// console.log(pointlength);
 
 	if(pointlength < 10) {
-		console.log("frog!");
 		_path.add(destination);
 		lineInc +=1;
 		rndpoint = point2 + (new Point(getRandomA(-1, 1),getRandomA(-1, 1))* 10);
@@ -259,22 +281,35 @@ function drawSquiggle(_path) {
 	} else {
 		var direction = destination - point2;
 		var d1 = direction.normalize();
-		var point3 = point2 + (d1*8);
+		var point3 = point2 + (d1*10);
 
 		_path.segments[lineInc].point = point3;
 	}
 
 }
 
-function onFrame(event) {
-	drawSquiggle(mainPath);
+// var squiggleCount = 0;
 
-	// for(var i = 0; i < mainPath.segments.length; i++) {
-	// 	mainPath.segments[i].point += (new Point(getRandomA(-1, 1),getRandomA(-1, 1))*2);
+function onFrame(event) {
+	drawSquiggle(mainPaths[mainPaths.length-1]);
+
+	// for(var i = 0; i < mainPaths[mainPaths.length-1].segments.length; i++) {
+	// 	mainPaths[mainPaths.length-1].segments[i].point += (new Point(getRandomA(-1, 1),getRandomA(-1, 1))*2);
 	// }
 
 }
 
 function onMouseMove(event) {
+
+}
+
+function onKeyDown(event) {
+	lineInc =0;
+	makeNewSquiggle();
+	// squiggleCount +=1;
+
+	if(event.key == 'a') {
+		console.log(mainPaths);
+	}
 
 }
